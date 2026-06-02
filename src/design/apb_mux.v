5184b4cc19e2dc33aa248a1c1708c666e683db60
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-06-02 09:31:15 +0530

    Refactor apb_mux instantiation into module definition

---
+++ src/design/apb_mux.v
@@ -0,0 +1,15 @@
+
+module apb_mux(input MST_PSEL,
+               output MST_PREADY,
+               output [7:0]MST_PRDATA,
+               
+               output SLV_PSEL1,
+               input SLV_PREADY1,
+               input [7:0]SLV_PRDATA1,
+
+               output SLV_PSEL2,
+               input SLV_PREADY2,
+               input [7:0]SLV_PRDATA2
+);
+
+endmodule
