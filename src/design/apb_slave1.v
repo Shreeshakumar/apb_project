18086b1bca17bd7a132dfbca9d49a8dbac64f543
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-06-02 15:27:55 +0530

    Refactor apb_slave1 module for memory operations
    Refactor apb_slave1 module to use reg for outputs and add memory functionality.

--- src/design/apb_slave1.v
+++ src/design/apb_slave1.v
@@ -1,13 +1,24 @@
 
-module apb_slave1(
+module apb_slave1(input PCLK,PRESETn,transfer,
+                  
                   input PENABLE,PWRITE,
                   input [7:0]PWDATA,PADDR,
                   input SLV_PSEL1,
-                  output SLV_PREADY1,
-                  output [7:00]SLV_PRDATA1
+                  output reg SLV_PREADY1,
+                  output reg [7:00]SLV_PRDATA1
 );
 
-  always@(*)
-    
+    reg [7:0]mem[0:50];
+reg [2:0]CS,NS;
 
+    always@(PCLK)
+        begin
+            if(PENABLE && PWRITE && SLV_PSEL1 )
+                begin SLV_PREADY1 = 0; mem[PADDR] = PWDATA; end
+            else if (PENABLE && !PWRITE && SLV_PSEL1 )
+                begin SLV_PREADY1 = 0; SLV_PRDATA1 = mem[PADDR]; end
+            else
+                SLV_PREADY1 = 0;
+        end
+        
 endmodule
