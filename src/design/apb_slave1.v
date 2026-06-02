abba0b117c0ce03e49f2a74be1d6043569fd32e1
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-06-02 12:59:06 +0530

    Define apb_slave1 module with inputs and outputs
    Added module definition for apb_slave1 with inputs and outputs.

---
+++ src/design/apb_slave1.v
@@ -0,0 +1,13 @@
+
+module apb_slave1(
+                  input PENABLE,PWRITE,
+                  input [7:0]PWDATA,PADDR,
+                  input SLV_PSEL1,
+                  output SLV_PREADY1,
+                  output [7:00]SLV_PRDATA1
+);
+
+  always@(*)
+    
+
+endmodule
