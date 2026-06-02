9d6b8ea9456c649c7ee59c44ed9d51f5ee41a20b
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-06-02 10:30:51 +0530

    Update APB master module for address width changes

--- src/design/apb_master.v
+++ src/design/apb_master.v
@@ -1,13 +1,45 @@
 module apb_master(
                     input PCLK,PRESETn,transfer,READ_WRITE,
-                    input [7:0]apb_write_paddr,apb_write_data,apb_read_paddr,
+    input [8:0]apb_write_paddr,
+    input [7:0]apb_write_data,
+    input [8:0]apb_read_paddr,
                     output [7:0]apb_read_data_out,
                     output PENABLE,PWRITE,
-                    output [7:0]PWDATA,PADDR,
+                    output [7:0]PWDATA,
+    output [8:0]PADDR,
                     output PSEL,
                     input PREADY,
                     input [7:0]PRDATA
 );
 
+localparam IDLE = 0;
+localparam SETUP = 1;
+localparam ACCESS = 2;
+
+reg [1:0]CS,NS;
     
+always@(posedge PCLK or negedge PRESETN)
+    begin    CS <=(!PRESETn)?IDLE:NS;    end
+
+    always@(PCLK)
+        begin
+            case(CS)
+                IDLE    :    begin
+                                PSEL = 'd0;
+                                PENABLE = 'd0;
+                                NS = (transfer)?SETUP:IDLE;
+                            end
+                SETUP   :    begin
+                                PSEL = 'd1;
+                                PWRITE = 'd1;
+                                PADDR = apb_write_paddr;
+                                NS = ACCESS;
+                            end
+                ACCESS  :    begin
+                                PENABLE = 'd1;
+                    NS = (PREADY)? (transfer)? SETUP : IDLE : ACCESS ;
+                            end 
+                default :    NS = IDLE;
+            endcase
+        end
 endmodule
