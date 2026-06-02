fb4add5479684114864dde1984725b36450a321c
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-06-02 12:42:20 +0530

    Refactor apb_mux module to include clock and reset

--- src/design/apb_mux.v
+++ src/design/apb_mux.v
@@ -1,5 +1,8 @@
 
-module apb_mux(input MST_PSEL,
+module apb_mux(input PCLK,PRESETn,transfer,
+
+               input [8:0]PADDR,
+               input MST_PSEL,
                output MST_PREADY,
                output [7:0]MST_PRDATA,
                
@@ -12,4 +15,35 @@ module apb_mux(input MST_PSEL,
                input [7:0]SLV_PRDATA2
 );
 
+localparam IDLE = 4;
+localparam SETUP = 2;
+localparam ACCESS = 1;
+
+reg [2:0]CS,NS;
+    
+    always@(posedge PCLK or negedge PRESETn)
+    begin    CS <=(!PRESETn)?IDLE:NS;    end
+
+  always@(*)
+        begin
+            case(CS)
+                IDLE    :    begin  //SLV_PSEL1  SLV_PSEL2  MST_PREADY  MST_PRDATA
+                                SLV_PSEL1 =  'd0;
+                                SLV_PSEL2 =  'd0;
+                                MST_PREADY = 'd0;
+                                NS = (transfer)?SETUP:IDLE;
+                            end
+                SETUP   :    begin
+                                PSEL1 = (PADDR[8])? 'd0:'d1;
+                                PSEL2 = (PADDR[8])? 'd1:'d0;
+                                MST_PREADY = 'd0;
+                                NS = ACCESS;
+                            end
+                ACCESS  :    begin
+                                MST_PREADY = 'd1;
+                                NS = (transfer)? SETUP : IDLE ;
+                            end 
+                default :    NS = IDLE;
+            endcase
+        end
 endmodule
