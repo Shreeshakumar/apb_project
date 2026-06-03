87d62831f7f09691ffa803af80fd86342c7f962e
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-06-03 10:07:49 +0530

    Simplify apb_mux state machine logic
    Refactor apb_mux module by removing unused states and simplifying the state machine logic.

--- src/design/apb_mux.v
+++ src/design/apb_mux.v
@@ -1,7 +1,5 @@
 
-module apb_mux(input PCLK,PRESETn,transfer,
-
-               input [8:0]PADDR,
+module apb_mux(input [8:0]PADDR,
                input MST_PSEL,
                output MST_PREADY,
                output [7:0]MST_PRDATA,
@@ -15,36 +13,13 @@ module apb_mux(input PCLK,PRESETn,transfer,
                input [7:0]SLV_PRDATA2
 );
 
-localparam IDLE = 4;
-localparam SETUP = 2;
-localparam ACCESS = 1;
-
-reg [2:0]CS,NS;
-    
-    always@(posedge PCLK or negedge PRESETn)
-    begin    CS <=(!PRESETn)?IDLE:NS;    end
-
   always@(*)
         begin
-            case(CS)
-                IDLE    :    begin  //SLV_PSEL1  SLV_PSEL2  MST_PREADY  MST_PRDATA
-                                SLV_PSEL1 =  'd0;
-                                SLV_PSEL2 =  'd0;
-                                MST_PREADY = 'd0;
-                                NS = (transfer)?SETUP:IDLE;
-                            end
-                SETUP   :    begin
+          if (PSEL)
+            begin
                                 PSEL1 = (PADDR[8])? 'd0:'d1;
                                 PSEL2 = (PADDR[8])? 'd1:'d0;
-                                MST_PREADY = 'd0;
-                                NS = ACCESS;
-                            end
-                ACCESS  :    begin
+                                MST_PREADY = (PADDR[8])? SLV_PREADY2:SLV_PREADY1;
                                 MST_PRDATA = (PADDR[8])? SLV_PRDATA2 : SLV_PRDATA1;
-                                MST_PREADY = 'd1;
-                                NS = (transfer)? SETUP : IDLE ;
-                            end 
-                default :    NS = IDLE;
-            endcase
-        end
+            end end
 endmodule
